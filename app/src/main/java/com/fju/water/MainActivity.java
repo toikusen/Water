c854b5f2cc776b948fa82862c084df8527291fdf
miia <miia@com.tw>
2019-12-03 16:26:23 +0800

    notepad++ 下拉選單

--- app/src/main/java/com/fju/water/MainActivity.java
+++ app/src/main/java/com/fju/water/MainActivity.java
@@ -12,17 +12,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName() ;
     private EditText edMonth;
 
     boolean isNext = false;
@@ -33,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.d(TAG, "onCreate");
         edMonth = findViewById(R.id.month);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -45,15 +49,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         Switch sw = findViewById(R.id.sw);
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 isNext = isChecked;
                 TextView text = findViewById(R.id.type);
-                text.setText(isNext ? getString(R.string.every_other_month) : getString(R.string.monthly));
+                text.setText(isNext ? getString(R.string.every_other_month) :getString(R.string.monthly));
             }
         });
+
+        //spinner
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +85,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
 
     public void reset(){
         String message = "";
@@ -81,84 +136,59 @@ public class MainActivity extends AppCompatActivity {
                 reset();
             }
         };
+
         if (!TextUtils.isEmpty(edMonth.getText().toString())) {
             float num = Float.parseFloat(edMonth.getText().toString());
             double money = 0;
-            if (num >= 1 && num <= 10) {
-                money = num * 7.35;
+            if (isNext = false) {
+                if (num >= 1 && num <= 10) {
+                    money = num * 7.35;
 //                new AlertDialog.Builder(MainActivity.this)
 //                        .setTitle("每月抄表費用")
 //                        .setMessage("費用"+ money)
 //                        .setPositiveButton("OK",listner)
 //                        .show();
-//            }else if(num>=11 && num<=30){
-//                    money= num*9.45-21;
+                } else if (num >= 11 && num <= 30) {
+                    money = num * 9.45 - 21;
 //                  new AlertDialog.Builder(MainActivity.this)
 //                        .setTitle("每月抄表費用")
 //                        .setMessage("費用"+ money)
 //                        .show();
-//              }else if(num>=31 && num<=50){
-//                    money= num*11.55-84;
+                } else if (num >= 31 && num <= 50) {
+                    money = num * 11.55 - 84;
 //                 new AlertDialog.Builder(MainActivity.this)
 //                          .setTitle("每月抄表費用")
 //                            .setMessage("費用"+ money)
 //                         .setPositiveButton("OK",listner)
 //                         .show();
-//                }else if (num>50){
-//                    money= num*12.075-110.25;
+                } else if (num > 50) {
+                    money = num * 12.075 - 110.25;
 //                  new AlertDialog.Builder(MainActivity.this)
 //                           .setTitle("每月抄表費用")
 //                           .setMessage("費用"+ money)
 //                           .setPositiveButton("OK",listner)
 //                            .show();
-//                }
+                }
 
-                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                intent.putExtra(getString(R.string.extra_money), money);
-                startActivity(intent);
-
-//            } else if (TextUtils.isEmpty(edMonth.getText().toString()) && !TextUtils.isEmpty(edNext.getText().toString())) {
-//                float number = Float.parseFloat(edNext.getText().toString());
-//                if (number >= 1 && number <= 20) {
-//                    money = number * 7.35;
-//                    new AlertDialog.Builder(MainActivity.this)
-//                            .setTitle("隔月抄表費用")
-//                            .setMessage("費用" + money)
-//                            .setPositiveButton("OK", listner)
-//                            .show();
-//                } else if (number >= 21 && number <= 60) {
-//                    money = number * 9.45 - 42;
-//                    new AlertDialog.Builder(MainActivity.this)
-//                            .setTitle("隔月抄表費用")
-//                            .setMessage("費用" + money)
-//                            .setPositiveButton("OK", listner)
-//                            .show();
-//                } else if (number >= 61 && number <= 100) {
-//                    money = number * 11.55 - 168;
-//                    new AlertDialog.Builder(MainActivity.this)
-//                            .setTitle("隔月抄表費用")
-//                            .setMessage("費用" + money)
-//                            .setPositiveButton("OK", listner)
-//                            .show();
-//                } else if (number > 100) {
-//                    money = number * 12.075 - 220.5;
-//                    new AlertDialog.Builder(MainActivity.this)
-//                            .setTitle("隔月抄表費用")
-//                            .setMessage("費用" + money)
-//                            .setPositiveButton("OK", listner)
-//                            .show();
-//                }
-//            } else if (TextUtils.isEmpty(edMonth.getText().toString()) && TextUtils.isEmpty(edNext.getText().toString())) {
-//                new AlertDialog.Builder(MainActivity.this)
-//                        .setTitle("錯誤")
-//                        .setMessage("請輸入數值")
-//                        .setPositiveButton("OK", listner)
-//                        .show();
-//
+            } else {
+                if (num >= 1 && num <= 20) {
+                    money = num * 7.35;
+                } else if (num >= 21 && num <= 60) {
+                    money = num * 9.45 - 42;
+                } else if (num >= 61 && num <= 100) {
+                    money = num * 11.55 - 168;
+                } else if (num > 100) {
+                    money = num * 12.075 - 220.5;
+                }
             }
-
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtra(getString(R.string.extra_money), money);
+            startActivity(intent);
         }
+
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
